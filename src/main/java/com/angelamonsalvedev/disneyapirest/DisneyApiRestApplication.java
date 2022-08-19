1b4656262d55ac1843697af8522911222aa3215f
Angela Monsalve <angelapmonsalve@gmail.com>
2022-08-19 09:32:44 -0500

    Archivos iniciales del proyecto

---
+++ src/main/java/com/angelamonsalvedev/disneyapirest/DisneyApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.angelamonsalvedev.disneyapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyApiRestApplication.class, args);
+	}
+
+}
