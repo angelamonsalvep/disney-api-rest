c4d2743bf3d5534a616a71e952c959fbb558062e
Angela Monsalve <angelapmonsalve@gmail.com>
2022-08-20 15:23:22 -0500

    Se crea clase para el manejo de excepciones

---
+++ src/main/java/com/angelamonsalvedev/disneyapirest/exception/ResourceNotFoundException.java
@@ -0,0 +1,21 @@
+package com.angelamonsalvedev.disneyapirest.exception;
+
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Data
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+
+    private String nombreDelRecurso;
+    private String nombreDelCampo;
+    private Long valorDelCampo;
+
+    public ResourceNotFoundException(String nombreDelRecurso, String nombreDelCampo, Long valorDelCampo) {
+        super(String.format("%$ no encontrado con: %$ : '%$' ", nombreDelRecurso, nombreDelCampo, valorDelCampo));
+        this.nombreDelRecurso = nombreDelRecurso;
+        this.nombreDelCampo = nombreDelCampo;
+        this.valorDelCampo = valorDelCampo;
+    }
+}
